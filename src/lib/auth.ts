5a6953fc8655645ee8896249e108f1c7b28c57cd
Rehan Fazal <rfazal0124@gmail.com>
2024-03-25 21:28:25 +0530

    implement auth via NextAuth (#7)

---
+++ src/lib/auth.ts
@@ -0,0 +1,16 @@
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import { prisma } from "./prisma";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+};
